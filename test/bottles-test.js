698c2471346b47263cedf6b2fd208f8fb4b41c87
Sebastian Yandun <svyandun@gmail.com>
2017-06-22 20:26:50 -0500

    Pass a couple of verses test

--- test/bottles-test.js
+++ test/bottles-test.js
@@ -39,7 +39,7 @@ Go to the store and buy some more, 99 bottles of beer on the wall.`;
     expect(Bottles.verse(0)).to.equal(expected);
   });
 
-  it.skip('test a couple verses', () => {
+  it('test a couple verses', () => {
     const expected = `99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
 
