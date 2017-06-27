50988b9fbe64454dfad15d8dc2a7e9fed81fd16c
Sebastian Yandun <svyandun@gmail.com>
2017-06-26 21:23:28 -0500

    Pass whole song test

--- test/bottles-test.js
+++ test/bottles-test.js
@@ -62,7 +62,7 @@ Go to the store and buy some more, 99 bottles of beer on the wall.`;
     expect(Bottles.verses(2, 0)).to.equal(expected);
   });
 
-  it.skip('test the whole song', () => {
+  it('test the whole song', () => {
     const expected = `99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
 
